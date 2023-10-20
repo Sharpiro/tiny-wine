55b9e4077a7d2ea94b95f5f82903ada54aa061b4
sharpiro <dsharpbb09@gmail.com>
2023-10-20 11:05:28 -0400

    calling print inline

--- test_program/test.asm
+++ test_program/test.asm
@@ -1,22 +1,43 @@
 BITS 64
 
-SYS_WRITE equ 1
-SYS_EXIT equ 60
-STDOUT equ 1
+SYS_WRITE equ 0x01
+SYS_EXIT equ 0x3c
+STDOUT equ 0x01
 
 _start:
+    ; print inline
+    mov rax, 0x6363636363636363
+    push rax
     mov rax, SYS_WRITE
     mov rdi, STDOUT
-    mov rsi, msg
-    mov rdx, msg_len
+    ; dereferences?
+    ; mov rsi, [rsp-4]
+    mov rsi, rsp
+    mov rdx, 8
     syscall
 
+    ; ; print msg
+    ; mov rax, SYS_WRITE
+    ; mov rdi, STDOUT
+    ; mov rsi, msg
+    ; mov rdx, msg_len
+    ; syscall
+
+    ; ; print msg_two
+    ; mov rax, SYS_WRITE
+    ; mov rdi, STDOUT
+    ; mov rsi, msg_two
+    ; mov rdx, msg_two_len
+    ; syscall
+
     mov rax, SYS_EXIT
-    xor rdi, rdi
+    mov rdi, 0
     syscall
 
 section .text
     global _start
 section .data
-    msg db `Hello, Wine\n`
+    msg db "aaaaaaaaaaaaaaaa"
     msg_len equ $ - msg
+    msg_two db "bbbbbbbbbbbbbbbb"
+    msg_two_len equ $ - msg_two
