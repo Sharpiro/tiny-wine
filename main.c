09331ffa78e40ad3cdce1fe75692fc29edba9d54
sharpiro <dsharpbb09@gmail.com>
2023-12-02 11:53:28 -0500

    split out asm calls

--- main.c
+++ main.c
@@ -12,13 +12,16 @@ static void run_asm(u_int64_t value, uint64_t program_entry);
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        fprintf(stderr, "Usage: linker <filename>\n");
+        printf("Usage: tiny_wine <filename>\n");
         exit(EXIT_FAILURE);
     }
 
     char *program_path = argv[1];
     struct stat file_stat;
-    stat(program_path, &file_stat);
+    if (stat(program_path, &file_stat)) {
+        printf("file not found\n");
+        return 1;
+    }
     printf("size: %ld\n", file_stat.st_size);
 
     void *program_map_buffer = mmap(
@@ -37,10 +40,6 @@ int main(int argc, char *argv[]) {
     printf("mmap success\n");
 
     FILE *file = fopen(program_path, "r");
-    if (!file) {
-        printf("file not found\n");
-        return 1;
-    }
 
     fread(program_map_buffer, 1, file_stat.st_size, file);
     printf("read success\n");
@@ -58,43 +57,40 @@ int main(int argc, char *argv[]) {
 
     printf("starting child program...\n\n");
     run_asm(stack_start, header->e_entry);
-    printf("child program complete\n");
-
-    int error = munmap(program_map_buffer, PROGRAM_SPACE_SIZE);
-    if (error) {
-        printf("munmap failed\n");
-        return error;
-    }
 }
 
 static void run_asm(uint64_t stack_start, uint64_t program_entry) {
-    asm volatile(
-        "mov rbx, 0x00;"
-        // set stack pointer
-        "mov rsp, %[stack_start];"
+    asm volatile("mov rbx, 0x00;"
+                 // set stack pointer
+                 "mov rsp, %[stack_start];"
 
-        //  clear 'PF' flag
-        "mov r15, 0xff;"
-        "xor r15, 1;"
+                 //  clear 'PF' flag
+                 "mov r15, 0xff;"
+                 "xor r15, 1;"
 
-        // clear registers
-        //  "mov rax, 0x00;"
-        "mov rcx, 0x00;"
-        "mov rdx, 0x00;"
-        "mov rsi, 0x00;"
-        "mov rdi, 0x00;"
-        "mov rbp, 0x00;"
-        "mov r8, 0x00;"
-        "mov r9, 0x00;"
-        "mov r10, 0x00;"
-        "mov r11, 0x00;"
-        "mov r12, 0x00;"
-        "mov r13, 0x00;"
-        "mov r14, 0x00;"
-        "mov r15, 0x00;"
+                 // clear registers
+                 "mov rax, 0x00;"
+                 "mov rcx, 0x00;"
+                 "mov rdx, 0x00;"
+                 "mov rsi, 0x00;"
+                 "mov rdi, 0x00;"
+                 //  "mov rbp, 0x00;"
+                 "mov r8, 0x00;"
+                 "mov r9, 0x00;"
+                 "mov r10, 0x00;"
+                 "mov r11, 0x00;"
+                 "mov r12, 0x00;"
+                 "mov r13, 0x00;"
+                 "mov r14, 0x00;"
+                 "mov r15, 0x00;"
+                 :
+                 : [stack_start] "r"(stack_start));
 
-        // jump to program
-        "jmp %[program_entry];"
-        :
-        : [program_entry] "r"(program_entry), [stack_start] "r"(stack_start));
+    // jump to program
+    asm volatile("jmp %[program_entry];"
+                 :
+                 : [program_entry] "r"(program_entry)
+                 //
+                 // );
+                 : "rax");
 }
--- test_program_asm/build.sh
+++ test_program_asm/build.sh
@@ -6,15 +6,15 @@ echo "compiling"
 nasm -g -o std_lib.o -l std_lib.lst -f elf64 std_lib.asm
 nasm -g -o test.o -l test.lst -f elf64 test.asm
 
-echo "linking shared lib"
-ld -shared -o libstdasm.so std_lib.o
+# echo "linking shared lib"
 # gcc -shared -o libstdasm.so std_lib.o
-# gcc -Wall -nostdlib -o test.exe ./libstdasm.so test.o 
 echo "linking executable"
-gcc -nostartfiles -Wall -o test.exe test.o ./libstdasm.so 
+# ld -shared -o libstdasm.so std_lib.o
+# gcc -Wall -nostdlib -o test.exe ./libstdasm.so test.o 
+# gcc -nostartfiles -Wall -o test.exe test.o ./libstdasm.so 
 
 # nasm -o test.bin -f bin test.asm
-# ld -o test.exe std_lib.o test.o
+ld -o test.exe std_lib.o test.o
 # ld -o test.exe test.o ./libstdasm.so 
 # cp test.exe test.exe.cpy
 # ./test.exe hello_sailor
