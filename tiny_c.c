bb444f9710aed470d3f9e4b59dd51904c456f33f
sharpiro <dsharpbb09@gmail.com>
2024-01-11 16:33:07 -0500

    use size_t instead of 64bit, generic syscall params

--- tiny_c.c
+++ tiny_c.c
@@ -2,7 +2,6 @@
 #include <fcntl.h>
 #include <stdarg.h>
 #include <stdbool.h>
-#include <stdint.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/syscall.h>
@@ -12,25 +11,25 @@
 #define NO_LIBC 1
 
 struct SysArgs {
-    uint64_t rdi;
-    uint64_t rsi;
-    uint64_t rdx;
-    uint64_t rcx;
-    uint64_t r8;
-    uint64_t r9;
-    uint64_t r10;
+    size_t param_one;
+    size_t param_two;
+    size_t param_three;
+    size_t param_four;
+    size_t param_five;
+    size_t param_six;
+    size_t param_seven;
 };
 
-uint64_t tiny_c_syscall(uint64_t sys_no, struct SysArgs *sys_args) {
-    uint64_t result = 0;
+size_t tiny_c_syscall(size_t sys_no, struct SysArgs *sys_args) {
+    size_t result = 0;
 
-    asm("mov rdi, %0" : : "r"(sys_args->rdi));
-    asm("mov rsi, %0" : : "r"(sys_args->rsi));
-    asm("mov rdx, %0" : : "r"(sys_args->rdx));
-    asm("mov rcx, %0" : : "r"(sys_args->rcx));
-    asm("mov r8, %0" : : "r"(sys_args->r8));
-    asm("mov r9, %0" : : "r"(sys_args->r9));
-    asm("mov r10, %0" : : "r"(sys_args->r10));
+    asm("mov rdi, %0" : : "r"(sys_args->param_one));
+    asm("mov rsi, %0" : : "r"(sys_args->param_two));
+    asm("mov rdx, %0" : : "r"(sys_args->param_three));
+    asm("mov rcx, %0" : : "r"(sys_args->param_four));
+    asm("mov r8, %0" : : "r"(sys_args->param_five));
+    asm("mov r9, %0" : : "r"(sys_args->param_six));
+    asm("mov r10, %0" : : "r"(sys_args->param_seven));
     asm("mov rax, %0" : : "r"(sys_no));
     asm("syscall" : "=r"(result) : :);
 
@@ -39,15 +38,15 @@ uint64_t tiny_c_syscall(uint64_t sys_no, struct SysArgs *sys_args) {
 
 void tiny_c_print_len(const char *data, size_t size) {
     struct SysArgs args = {
-        .rdi = STDOUT,
-        .rsi = (uint64_t)data,
-        .rdx = size,
+        .param_one = STDOUT,
+        .param_two = (size_t)data,
+        .param_three = size,
     };
     tiny_c_syscall(SYS_write, &args);
 }
 
 void tiny_c_print(const char *data) {
-    int size = 0;
+    size_t size = 0;
     for (size_t i = 0; true; i++) {
         if (data[i] == 0x00) {
             size = i;
@@ -60,9 +59,9 @@ void tiny_c_print(const char *data) {
 
 void tiny_c_newline(void) {
     struct SysArgs args = (struct SysArgs){
-        .rdi = STDOUT,
-        .rsi = (uint64_t) "\n",
-        .rdx = 1,
+        .param_one = STDOUT,
+        .param_two = (size_t) "\n",
+        .param_three = 1,
     };
     tiny_c_syscall(SYS_write, &args);
 }
@@ -72,7 +71,7 @@ void tiny_c_puts(const char *data) {
     tiny_c_newline();
 }
 
-uint64_t tiny_c_pow(uint64_t x, uint64_t y) {
+size_t tiny_c_pow(size_t x, size_t y) {
     size_t product = 1;
     for (size_t i = 0; i < y; i++) {
         product *= x;
@@ -81,7 +80,7 @@ uint64_t tiny_c_pow(uint64_t x, uint64_t y) {
     return product;
 }
 
-void tiny_c_print_number(uint64_t num) {
+void tiny_c_print_number(size_t num) {
     const char *HEX_CHARS = "0123456789abcdef";
     const size_t MAX_DIGITS = 16;
 
@@ -89,27 +88,27 @@ void tiny_c_print_number(uint64_t num) {
     num_buffer[0] = '0';
     num_buffer[1] = 'x';
 
-    uint64_t current_base = tiny_c_pow(0x10, MAX_DIGITS - 1);
+    size_t current_base = tiny_c_pow(0x10, MAX_DIGITS - 1);
     for (size_t i = 0; i < MAX_DIGITS; i++) {
-        uint64_t digit = num / current_base;
+        size_t digit = num / current_base;
         size_t j = i + 2;
         num_buffer[j] = HEX_CHARS[digit];
-        uint64_t digit_value = digit * current_base;
+        size_t digit_value = digit * current_base;
         num -= digit_value;
         current_base /= 0x10;
     }
 
     struct SysArgs args = {
-        .rdi = STDOUT,
-        .rsi = (uint64_t)num_buffer,
-        .rdx = 18,
+        .param_one = STDOUT,
+        .param_two = (size_t)num_buffer,
+        .param_three = 18,
     };
     tiny_c_syscall(SYS_write, &args);
 }
 
 struct PrintItem {
-    uint64_t start;
-    uint64_t length;
+    size_t start;
+    size_t length;
     char formatter;
 };
 
@@ -164,7 +163,7 @@ void tiny_c_printf(const char *format, ...) {
             break;
         }
         case 'x': {
-            uint64_t data = va_arg(var_args, uint64_t);
+            size_t data = va_arg(var_args, size_t);
             tiny_c_print_number(data);
             break;
         }
@@ -183,48 +182,48 @@ void tiny_c_printf(const char *format, ...) {
     va_end(var_args);
 }
 
-void tiny_c_exit(int code) {
-    struct SysArgs args = {.rdi = code};
+void tiny_c_exit(size_t code) {
+    struct SysArgs args = {.param_one = code};
     tiny_c_syscall(SYS_exit, &args);
 }
 
 struct stat tiny_c_stat(const char *path) {
     struct stat file_stat;
     struct SysArgs args = {
-        .rdi = (uint64_t)path,
-        .rsi = (uint64_t)&file_stat,
+        .param_one = (size_t)path,
+        .param_two = (size_t)&file_stat,
     };
     tiny_c_syscall(SYS_stat, &args);
 
     return file_stat;
 }
 
-uint64_t tiny_c_fopen(const char *path) {
+size_t tiny_c_fopen(const char *path) {
     struct SysArgs args = {
-        .rdi = (uint64_t)path,
-        .rsi = O_RDWR,
+        .param_one = (size_t)path,
+        .param_two = O_RDWR,
     };
-    uint64_t fd = tiny_c_syscall(SYS_open, &args);
+    size_t fd = tiny_c_syscall(SYS_open, &args);
 
     return fd;
 }
 
-void tiny_c_fclose(uint64_t fd) {
+void tiny_c_fclose(size_t fd) {
     struct SysArgs args = {
-        .rdi = fd,
+        .param_one = fd,
     };
     tiny_c_syscall(SYS_close, &args);
 }
 
 void *tiny_c_mmap(size_t address, size_t length, size_t prot, size_t flags,
-                  uint64_t fd, size_t offset) {
+                  size_t fd, size_t offset) {
     struct SysArgs args = {
-        .rdi = address,
-        .rsi = length,
-        .rdx = prot,
-        .r10 = flags, // @note: disrespects calling convention
-        .r8 = fd,
-        .r9 = offset,
+        .param_one = address,
+        .param_two = length,
+        .param_three = prot,
+        .param_seven = flags, // @note: disrespects calling convention
+        .param_five = fd,
+        .param_six = offset,
     };
     void *result = (void *)tiny_c_syscall(SYS_mmap, &args);
 
@@ -233,8 +232,8 @@ void *tiny_c_mmap(size_t address, size_t length, size_t prot, size_t flags,
 
 size_t tiny_c_munmap(size_t address, size_t length) {
     struct SysArgs args = {
-        .rdi = address,
-        .rsi = length,
+        .param_one = address,
+        .param_two = length,
     };
     size_t result = tiny_c_syscall(SYS_munmap, &args);
 
